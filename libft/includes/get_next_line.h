bf0f012f62228e54f406e4829ef8f281675ec00f
Furkat MALLABAEV <fmallaba@e1r12p2.unit.ua>
2017-11-30 22:44:09 +0200

    move headers

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,16 +6,14 @@
 /*   By: fmallaba <fmallaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 18:35:50 by fmallaba          #+#    #+#             */
-/*   Updated: 2017/11/30 22:20:38 by fmallaba         ###   ########.fr       */
+/*   Updated: 2017/11/30 22:29:10 by fmallaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-
-# define BUFF_SIZE 1000
+# define BUFF_SIZE 10
 
 int					get_next_line(const int fd, char **line);
 
